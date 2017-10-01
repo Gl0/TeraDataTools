e06230601ca3280c975bd4818f4e7145538c12ae
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-10-01 14:52:25 +0300

    UI upgrade

--- DataCenterUnpack/UnpackForm.Designer.cs
+++ DataCenterUnpack/UnpackForm.Designer.cs
@@ -39,12 +39,25 @@
             this.InputFile = new System.Windows.Forms.TextBox();
             this.BrowseInput = new System.Windows.Forms.Button();
             this.FindKeyButton = new System.Windows.Forms.Button();
+            this.UnpackDCTab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.outputFile = new System.Windows.Forms.TextBox();
+            this.BrowseOutput = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.UnpackDCTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(8, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 13);
             this.label1.TabIndex = 0;
@@ -53,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Location = new System.Drawing.Point(8, 31);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(17, 13);
             this.label2.TabIndex = 1;
@@ -62,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Location = new System.Drawing.Point(8, 53);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 2;
@@ -70,11 +83,11 @@
             // 
             // Go
             // 
-            this.Go.Location = new System.Drawing.Point(15, 90);
+            this.Go.Location = new System.Drawing.Point(9, 108);
             this.Go.Name = "Go";
             this.Go.Size = new System.Drawing.Size(75, 23);
             this.Go.TabIndex = 4;
-            this.Go.Text = "go";
+            this.Go.Text = "Unpack";
             this.Go.UseVisualStyleBackColor = true;
             this.Go.Click += new System.EventHandler(this.Go_Click);
             // 
@@ -85,28 +98,28 @@
             // 
             // Key
             // 
-            this.Key.Location = new System.Drawing.Point(58, 19);
+            this.Key.Location = new System.Drawing.Point(54, 5);
             this.Key.Name = "Key";
             this.Key.Size = new System.Drawing.Size(329, 20);
             this.Key.TabIndex = 5;
             // 
             // IV
             // 
-            this.IV.Location = new System.Drawing.Point(58, 42);
+            this.IV.Location = new System.Drawing.Point(54, 28);
             this.IV.Name = "IV";
             this.IV.Size = new System.Drawing.Size(329, 20);
             this.IV.TabIndex = 6;
             // 
             // InputFile
             // 
-            this.InputFile.Location = new System.Drawing.Point(58, 64);
+            this.InputFile.Location = new System.Drawing.Point(54, 50);
             this.InputFile.Name = "InputFile";
             this.InputFile.Size = new System.Drawing.Size(296, 20);
             this.InputFile.TabIndex = 7;
             // 
             // BrowseInput
             // 
-            this.BrowseInput.Location = new System.Drawing.Point(360, 64);
+            this.BrowseInput.Location = new System.Drawing.Point(356, 50);
             this.BrowseInput.Name = "BrowseInput";
             this.BrowseInput.Size = new System.Drawing.Size(27, 22);
             this.BrowseInput.TabIndex = 9;
@@ -116,32 +129,133 @@
             // 
             // FindKeyButton
             // 
-            this.FindKeyButton.Location = new System.Drawing.Point(96, 90);
+            this.FindKeyButton.Location = new System.Drawing.Point(90, 108);
             this.FindKeyButton.Name = "FindKeyButton";
-            this.FindKeyButton.Size = new System.Drawing.Size(75, 23);
+            this.FindKeyButton.Size = new System.Drawing.Size(114, 23);
             this.FindKeyButton.TabIndex = 11;
-            this.FindKeyButton.Text = "Find Key";
+            this.FindKeyButton.Text = "Find Key in Memory";
             this.FindKeyButton.UseVisualStyleBackColor = true;
             this.FindKeyButton.Click += new System.EventHandler(this.FindKeyButton_Click);
+            // 
+            // UnpackDCTab
+            // 
+            this.UnpackDCTab.Controls.Add(this.tabPage1);
+            this.UnpackDCTab.Controls.Add(this.tabPage2);
+            this.UnpackDCTab.Location = new System.Drawing.Point(6, 6);
+            this.UnpackDCTab.Name = "UnpackDCTab";
+            this.UnpackDCTab.SelectedIndex = 0;
+            this.UnpackDCTab.Size = new System.Drawing.Size(400, 163);
+            this.UnpackDCTab.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.outputFile);
+            this.tabPage1.Controls.Add(this.BrowseOutput);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.InputFile);
+            this.tabPage1.Controls.Add(this.FindKeyButton);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.BrowseInput);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.IV);
+            this.tabPage1.Controls.Add(this.Go);
+            this.tabPage1.Controls.Add(this.Key);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(392, 137);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Unpack DC";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(392, 137);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Unpack DownloadedResources";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DownloadedResources.dat path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(342, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // outputFile
+            // 
+            this.outputFile.Location = new System.Drawing.Point(54, 76);
+            this.outputFile.Name = "outputFile";
+            this.outputFile.Size = new System.Drawing.Size(296, 20);
+            this.outputFile.TabIndex = 13;
+            // 
+            // BrowseOutput
+            // 
+            this.BrowseOutput.Location = new System.Drawing.Point(356, 76);
+            this.BrowseOutput.Name = "BrowseOutput";
+            this.BrowseOutput.Size = new System.Drawing.Size(27, 22);
+            this.BrowseOutput.TabIndex = 14;
+            this.BrowseOutput.Text = "...";
+            this.BrowseOutput.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Unpack";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 22);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // UnpackForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 121);
-            this.Controls.Add(this.FindKeyButton);
-            this.Controls.Add(this.BrowseInput);
-            this.Controls.Add(this.InputFile);
-            this.Controls.Add(this.IV);
-            this.Controls.Add(this.Key);
-            this.Controls.Add(this.Go);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(418, 181);
+            this.Controls.Add(this.UnpackDCTab);
             this.Name = "UnpackForm";
             this.Text = "DateCenter Unpacker";
+            this.UnpackDCTab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -158,6 +272,16 @@
         private System.Windows.Forms.TextBox InputFile;
         private System.Windows.Forms.Button BrowseInput;
         private System.Windows.Forms.Button FindKeyButton;
+        private System.Windows.Forms.TabControl UnpackDCTab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox outputFile;
+        private System.Windows.Forms.Button BrowseOutput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
